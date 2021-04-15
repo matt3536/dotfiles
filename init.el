fab3cb8f6b0ca77a0bbe5246e5de687aa1c98664
matt3536 <touhu1834@gmail.com>
2021-04-16 00:19:26 +0900

    first commit

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;;;load-pathes
+;;;;lod-pathes
 
 ;; load-pathを追加する関数を定義
 (defun add-to-load-path (&rest paths) (let (path) (dolist (path paths paths) (let ((default-directory
@@ -45,6 +45,9 @@
 (define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
 (define-key global-map (kbd "C-x ?") 'help-command)
 
+;; 過去に開いたファイルを選択して開く
+(global-set-key (kbd "C-c o") 'recentf-open-files)
+
 ;; 行番号を左側に常に表示
 ;(global-linum-mode t)
 (global-display-line-numbers-mode t)
@@ -55,7 +58,7 @@
 (setq-default indent-tabs-mode nil)
 
 ;; メニューバーの非表示
-nn(menu-bar-mode -1)
+(menu-bar-mode -1)
 
 ;; ツールバーの非表示
 (tool-bar-mode -1)
@@ -70,11 +73,183 @@ nn(menu-bar-mode -1)
 ;; 'save-xxxx'などのファイルを作成しない
 (setq auto-save-list-file-prefix nil)
 
+;; yes-no を y-nに変更
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; ビープ音とフラッシュを無効
+(setq ring-bell-function 'ignore)
+
+;; $PATHにfzfのパスを追加
+(setq exec-path (append exec-path '("/home/matt/.fzf/bin")))
+
 ;;;;  use-packages
 
+(use-package which-key
+  :diminish which-key-mode
+  :hook (after-init . which-key-mode))
+
+;;; all-the-icons
+(use-package all-the-icons
+  :if (display-graphic-p)
+  :config (unless (find-font (font-spec :name "all-the-icons"))
+            (all-the-icons-install-fonts t)))
+
+;;; ivy-swiper-counsel
+(use-package counsel
+  :after ivy
+  :delight
+  :bind (("M-x" . counsel-M-x)
+         ("M-y" . counsel-yank-pop)
+         ("C-x C-b" . counsel-ibuffer)
+         ("C-x C-l" . counsel-find-library)
+         ("C-x C-r" . counsel-recentf)
+         ("C-M-z" . counsel-fzf)
+         ("C-M-f" . counsel-ag))
+  :config (counsel-mode)        
+  :custom (counsel-rg-base-command "rg -S -M 150 --no-heading --line-number --color never %s"))
+
+(use-package ivy
+  :delight
+  :after ivy-rich
+  :bind (("C-x b" . ivy-switch-buffer)
+         ("C-x B" . ivy-switch-buffer-other-window)
+         ("M-H"   . ivy-resume)
+         :map ivy-minibuffer-map
+         ("<tab>" . ivy-alt-done)
+         ("C-i"   . ivy-partial-or-done)
+         ("S-SPC" . nil)
+         :map ivy-switch-buffer-map
+         ("C-k"   . ivy-switch-buffer-kill))
+  :custom
+  (ivy-case-fold-search-default t)
+  (ivy-count-format "(%d/%d) ")
+  (ivy-re-builders-alist '((t . ivy--regex-plus)))
+  (ivy-use-virtual-buffers t)
+  :config (ivy-mode))
+
+(use-package ivy-pass
+  :after ivy
+  :commands ivy-pass)
+
+(use-package ivy-rich
+  :defer 0.1
+  :preface
+  (defun ivy-rich-branch-candidate (candidate)
+    "Displays the branch candidate of the candidate for ivy-rich."
+    (let ((candidate (expand-file-name candidate ivy--directory)))
+      (if (or (not (file-exists-p candidate)) (file-remote-p candidate))
+          ""
+        (format "%s%s"
+                (propertize
+                 (replace-regexp-in-string abbreviated-home-dir "~/"
+                                           (file-name-directory
+                                            (directory-file-name candidate)))
+                 'face 'font-lock-doc-face)
+                (propertize
+                 (file-name-nondirectory
+                  (directory-file-name candidate))
+                 'face 'success)))))
+
+  (defun ivy-rich-compiling (candidate)
+    "Displays compiling buffers of the candidate for ivy-rich."
+    (let* ((candidate (expand-file-name candidate ivy--directory)))
+      (if (or (not (file-exists-p candidate)) (file-remote-p candidate)
+              (not (magit-git-repo-p candidate)))
+          ""
+        (if (my/projectile-compilation-buffers candidate)
+            "compiling"
+          ""))))
+
+  (defun ivy-rich-file-group (candidate)
+    "Displays the file group of the candidate for ivy-rich"
+    (let ((candidate (expand-file-name candidate ivy--directory)))
+      (if (or (not (file-exists-p candidate)) (file-remote-p cqandidate))
+          ""
+        (let* ((group-id (file-attribute-group-id (file-attributes candidate)))
+               (group-function (if (fboundp #'group-name) #'group-name #'identity))
+               (group-name (funcall group-function group-id)))
+          (format "%s" group-name)))))
+
+  (defun ivy-rich-file-modes (candidate)
+    "Displays the file mode of the candidate for ivy-rich."
+    (let ((candidate (expand-file-name candidate ivy--directory)))
+      (if (or (not (file-exists-p candidate)) (file-remote-p candidate))
+          ""
+        (format "%s" (file-attribute-modes (file-attributes candidate))))))
+
+  (defun ivy-rich-file-size (candidate)
+    "Displays the file size of the candidate for ivy-rich."
+    (let ((candidate (expand-file-name candidate ivy--directory)))
+      (if (or (not (file-exists-p candidate)) (file-remote-p candidate))
+          ""
+        (let ((size (file-attribute-size (file-attributes candidate))))
+          (cond
+           ((> size 1000000) (format "%.1fM " (/ size 1000000.0)))
+           ((> size 1000) (format "%.1fk " (/ size 1000.0)))
+           (t (format "%d " size)))))))
+
+  (defun ivy-rich-file-user (candidate)
+    "Displays the file user of the candidate for ivy-rich."
+    (let ((candidate (expand-file-name candidate ivy--directory)))
+      (if (or (not (file-exists-p candidate)) (file-remote-p candidate))
+          ""
+        (let* ((user-id (file-attribute-user-id (file-attributes candidate)))
+               (user-name (user-login-name user-id)))
+          (format "%s" user-name)))))
+
+  (defun ivy-rich-switch-buffer-icon (candidate)
+    "Returns an icon for the candidate out of `all-the-icons'."
+    (with-current-buffer
+        (get-buffer candidate)
+      (let ((icon (all-the-icons-icon-for-mode major-mode :height 0.9)))
+        (if (symbolp icon)
+            (all-the-icons-icon-for-mode 'fundamental-mode :height 0.9)
+          icon))))
+  :config
+  (plist-put ivy-rich-display-transformers-list
+             'counsel-find-file
+             '(:columns
+               ((ivy-rich-candidate               (:width 73))
+                (ivy-rich-file-user               (:width 8 :face font-lock-doc-face))
+                (ivy-rich-file-group              (:width 4 :face font-lock-doc-face))
+                (ivy-rich-file-modes              (:width 11 :face font-lock-doc-face))
+                (ivy-rich-file-size               (:width 7 :face font-lock-doc-face))
+                (ivy-rich-file-last-modified-time (:width 30 :face font-lock-doc-face)))))
+  (plist-put ivy-rich-display-transformers-list
+             'counsel-projectile-switch-project
+             '(:columns
+               ((ivy-rich-branch-candidate        (:width 80))
+                (ivy-rich-compiling))))
+  (plist-put ivy-rich-display-transformers-list
+             'ivy-switch-buffer
+             '(:columns
+               ((ivy-rich-switch-buffer-icon       (:width 2))
+                (ivy-rich-candidate                (:width 40))
+                (ivy-rich-switch-buffer-size       (:width 7))
+                (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
+                (ivy-rich-switch-buffer-major-mode (:width 20 :face warning)))
+               :predicate (lambda (cand) (get-buffer cand))))
+  (ivy-rich-mode 1))
+
+(use-package all-the-icons-ivy
+  :after (all-the-icons ivy)
+  :custom (all-the-icons-ivy-buffer-commands '(ivy-switch-buffer-other-window))
+  :config
+  (add-to-list 'all-the-icons-ivy-file-commands 'counsel-dired-jump)
+  (add-to-list 'all-the-icons-ivy-file-commands 'counsel-find-library)
+  (all-the-icons-ivy-setup))
+
 (use-package swiper
-  :bind
-  ("M-s M-s" . swiper-thing-at-point))
+  :after ivy
+  :bind (("C-s" . swiper)
+         :map swiper-map
+         ("M-%" . swiper-query-replace))
+  		 ("M-s M-s" . swiper-thing-at-point))
+          
+;(use-package swiper
+;  :ensure t
+;  :bind
+;  ("M-s M-s" . swiper-thing-at-point))
  
 (use-package doom-themes
   :custom
@@ -86,10 +261,6 @@ nn(menu-bar-mode -1)
   (load-theme 'doom-dracula t)
   (doom-themes-neotree-config)
   (doom-themes-org-config))
-
-(use-package all-the-icons
-  :custom
-  (all-the-icons-scale-factor 1.0))
 
 (use-package hide-mode-line
   :hook
@@ -116,25 +287,15 @@ nn(menu-bar-mode -1)
 
 
 (use-package paren
-   :ensure nil
-   :hook
-   (after-init . show-paren-mode)
-   :custom-face
-   (show-paren-match ((nil (:background "#44475a" :foreground "#f1fa8c"))))
-   :custom
-   (show-paren-style 'mixed)
-   (show-paren-when-point-inside-paren t)
-   (show-paren-when-point-in-periphery t))
-
-;(use-package neotree
-;    :after
-;    projectile
-;    :commands
-;    (neotree-show neotree-hide neotree-dir neotree-find)
-;    :custom
-;   (neo-theme 'nerd2)
-;   :bind
-;   ("[f9]" . neotree-toggle))
+  :ensure nil
+  :hook
+  (after-init . show-paren-mode)
+  :custom-face
+  (show-paren-match ((nil (:background "#44475a" :foreground "#f1fa8c"))))
+  :custom
+  (show-paren-style 'mixed)
+  (show-paren-when-point-inside-paren t)
+  (show-paren-when-point-in-periphery t))
 
 (use-package neotree
   :commands
@@ -144,35 +305,34 @@ nn(menu-bar-mode -1)
   :bind
   ("<f9>" . neotree-toggle))
 
-
 (use-package highlight-indent-guides
-    :diminish
-    :hook
-    ((prog-mode yaml-mode) . highlight-indent-guides-mode)
-    :custom
-    (highlight-indent-guides-auto-enabled t)
-    (highlight-indent-guides-responsive t)
-    (highlight-indent-guides-method 'character)) ; column
+  :diminish
+  :hook
+  ((prog-mode yaml-mode) . highlight-indent-guides-mode)
+  :custom
+  (highlight-indent-guides-auto-enabled t)
+  (highlight-indent-guides-responsive t)
+  (highlight-indent-guides-method 'character)) ; column
 
  (use-package beacon
-    :custom
-    (beacon-color "yellow")
-    :config
-    (beacon-mode 1))
+  :custom
+  (beacon-color "yellow")
+  :config
+  (beacon-mode 1))
 
  (use-package dashboard
-   :diminish
-   (dashboard-mode page-break-lines-mode)
-   :custom
-   (dashboard-startup-banner "~/.emacs/dashboard.txt")
-   (dashboard-items '((recents . 15)
-              (projects . 5)
-              (bookmarks . 5)
-              (agenda . 5)))
-   :hook
-   (after-init . dashboard-setup-startup-hook)
-   :config
-   (add-to-list 'dashboard-items '(agenda) t))
+  :diminish
+  (dashboard-mode page-break-lines-mode)
+  :custom
+  (dashboard-startup-banner "~/.emacs/dashboard.txt")
+  (dashboard-items '((recents . 15)
+                     (projects . 5)
+                     (bookmarks . 5)
+                     (agenda . 5)))
+  :hook
+  (after-init . dashboard-setup-startup-hook)
+  :config
+  (add-to-list 'dashboard-items '(agenda) t))
 
 (use-package magit
   :ensure t
@@ -183,13 +343,28 @@ nn(menu-bar-mode -1)
   )
 
 (use-package git-gutter
-    :custom
-    (git-gutter:modified-sign "~")
-    (git-gutter:added-sign    "+")
-    (git-gutter:deleted-sign  "-")
-    :custom-face
-    (git-gutter:modified ((t (:background "#f1fa8c"))))
-    (git-gutter:added    ((t (:background "#50fa7b"))))
-    (git-gutter:deleted  ((t (:background "#ff79c6"))))
-    :config
-    (global-git-gutter-mode +1))
+  :custom
+  (git-gutter:modified-sign "~")
+  (git-gutter:added-sign    "+")
+  (git-gutter:deleted-sign  "-")
+  :custom-face
+  (git-gutter:modified ((t (:background "#f1fa8c"))))
+  (git-gutter:added    ((t (:background "#50fa7b"))))
+  (git-gutter:deleted  ((t (:background "#ff79c6"))))
+  :config
+  (global-git-gutter-mode +1))
+
+(use-package rainbow-mode
+  :init
+  (add-hook 'css-mode-hook 'rainbow-mode)
+  (add-hook 'html-mode-hook 'rainbow-mode)
+  (add-hook 'emacs-lisp-mode-hook 'rainbow-mode))
+
+;;;; hydra setting
+
+(defhydra hydra-zoom (global-map "<f2>")
+  "zoom"
+  ("g" text-scale-increase "in")
+  ("l" text-scale-decrease "out"))
+
+(executable-find "fzf")
